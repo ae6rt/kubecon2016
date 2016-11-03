b56281a7455e6a10dc6f8dffa10f4dee3c26a55b
Mark Petrovic <mpetrovic@paypal.com>
2016-11-03 15:56:41 -0700

    update dnsmasq entrypoint

--- dnsmasq/entrypoint.sh
+++ dnsmasq/entrypoint.sh
@@ -4,12 +4,7 @@ set -uxe
  
 env | sort
  
-CURL_OPTS="-s --retry 5 --retry-delay 5"
-TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
-NAMESPACE=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
-
 NODE_IP=$(ip -oneline -family inet addr show dev eth0 | awk '{split($4, a, "/"); printf a[1]}')
 
-DNS_PORT=$(curl ${CURL_OPTS} -H"Authorization: Bearer $TOKEN" https://${KUBERNETES_SERVICE_HOST}/api/v1/namespaces/kube-system/services/kube-dns | jq -r '.spec.ports[] | select(.protocol=="UDP") | .nodePort')
-
-dnsmasq --no-daemon --cache-size=1000 --no-resolv --address=/xoomapi/${NODE_IP} --server=${NODE_IP}#${DNS_PORT}
+# 169.254.8.53 is the kube-dns clusterIP
+dnsmasq --no-daemon --cache-size=1000 --no-resolv --address=/xoomapi/${NODE_IP} --server=169.254.8.53#53
