ab0e1c294d5fc4df0b9b615a21248fd421ba1858
Mark Petrovic <mpetrovic@paypal.com>
2016-11-04 12:45:06 -0700

    create a variable for kubedns cluster ip

--- dnsmasq/entrypoint.sh
+++ dnsmasq/entrypoint.sh
@@ -6,5 +6,6 @@ env | sort
  
 NODE_IP=$(ip -oneline -family inet addr show dev eth0 | awk '{split($4, a, "/"); printf a[1]}')
 
-# 169.254.8.53 is the kube-dns clusterIP
-dnsmasq --no-daemon --cache-size=1000 --no-resolv --address=/xoomapi/${NODE_IP} --server=169.254.8.53#53
+KUBE_DNS_CLUSTER_IP=169.254.8.53
+
+dnsmasq --no-daemon --cache-size=1000 --no-resolv --address=/xoomapi/${NODE_IP} --server=${KUBE_DNS_CLUSTER_IP}#53
